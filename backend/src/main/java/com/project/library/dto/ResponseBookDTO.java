d50528076298ca51b31289e7c0c6eda477dabcfa
Luís Parente <parente.lgsp@outlook.com>
2026-05-13 13:47:06 -0300

    Adiciona documentação utilizando OpenAPI

--- backend/src/main/java/com/project/library/dto/ResponseBookDTO.java
+++ backend/src/main/java/com/project/library/dto/ResponseBookDTO.java
@@ -3,9 +3,11 @@ package com.project.library.dto;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record ResponseBookDTO(
-        UUID id,
-        String title,
-        String author,
-        LocalDate publishedDate) {
+                @Schema(description = "Unique identifier of the book") UUID id,
+                @Schema(description = "Title of the book") String title,
+                @Schema(description = "Author of the book") String author,
+                @Schema(description = "Publication date of the book") LocalDate publishedDate) {
 }
